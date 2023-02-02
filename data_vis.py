2d9767667841e1639f5c25fc3cec2c4140aaf4f4
Samuel Looper <slooper@noteworthy.ai>
2023-02-02 14:47:52 +0100

    new motion planning library for python-based motion planning evals, many misc. fixes

--- data_vis.py
+++
@@ -1,5 +0,0 @@
-from src.utils.visualize import visualize_voxels, visualize_pointcloud, visualize_data
-from vedo import *
-if __name__ == "__main__":
-    pcloud = "/home/sam/thesis/asldoc-2022-MT-Samuel-Neural-Planning/data/pcl/livingroom1_1.0_000_000.ply"
-    visualize_pointcloud(pcloud)
